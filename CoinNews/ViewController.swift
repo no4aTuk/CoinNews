6a4e0e371a2867c4002da6e74e423f118b1fabd8
itsmuffintime <pochati@yandex.ru>
2019-06-13 19:35:06 +0300

    Initial Commit

---
+++ CoinNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoinNews
+//
+//  Created by Vonny on 13/06/2019.
+//  Copyright © 2019 Vonny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
