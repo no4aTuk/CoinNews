62d8a1de7d8204704ae711b74c506030e83f2829
itsmuffintime <pochati@yandex.ru>
2019-06-16 19:22:11 +0300

    font extension

---
+++ CoinNews/Helpers/Extenstions/UI/UIFont+Custom.swift
@@ -0,0 +1,23 @@
+//
+//  UIFont+Custom.swift
+//  CoinNews
+//
+//  Created by Vonny on 16/06/2019.
+//  Copyright © 2019 Vonny. All rights reserved.
+//
+
+import Foundation
+
+extension UIFont {
+    
+    enum FontStyle: String {
+        case regular = "TrebuchetMS"
+        case bold = "TrebuchetMS-Bold"
+        case boldItalic = "Trebuchet-BoldItalic"
+        case italic = "TrebuchetMS-Italic"
+    }
+    
+    static func appFont(_ fontName: FontStyle, size: CGFloat) -> UIFont {
+        return UIFont(name: fontName.rawValue, size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+}
